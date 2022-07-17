0f6f44e36dca9a29568e94748fdc30ff17e7463b
Sherzod <daminovsherzod2002@gmail.com>
2022-07-17 22:50:46 +0500

    get_users_data

--- get_users_data.py
+++ get_users_data.py
@@ -1,4 +1,4 @@
-import get_data
+from get_data import get_data
 
 def get_users_data(data:dict) -> list:
     """
@@ -12,3 +12,17 @@ def get_users_data(data:dict) -> list:
     Returns:
         list: users data list
     """
+    users = []
+    results = data['results']
+    for i in results:
+        name = i['name']
+        # a = name['first'] +' '+ name['last'] +' '+ i['phone']
+        users.append({
+            "first_name": name['first'],
+            "last_name": name['last'],
+            "phone_number": i['phone']
+        })
+    return users    
+
+data = get_data('randomuser_data.json')
+print(get_users_data(data))
